6bbbd553a5e56ebbe6e79c00360a1ca9cff61e8c
Ivan Abramovich <vanyaa2@mail.ru>
2026-08-16 15:29:58 +0700

    added mason

--- plugin/40_plugins.lua
+++ plugin/40_plugins.lua
@@ -506,10 +506,10 @@ end)
 -- If you need them to work elsewhere, consider using other package managers.
 --
 -- You can use it like so:
--- now_if_args(function()
---   add({ 'https://github.com/mason-org/mason.nvim' })
---   require('mason').setup()
--- end)
+now_if_args(function()
+  add({ 'https://github.com/mason-org/mason.nvim' })
+  require('mason').setup()
+end)
 
 -- Beautiful, usable, well maintained color schemes outside of 'mini.nvim' and
 -- have full support of its highlight groups. Use if you don't like 'miniwinter'
