8ef2d2f74171e539719016b15e3b6bcf9b0abe59
Ivan Abramovich <vanyaa2@mail.ru>
2026-08-16 15:30:33 +0700

    fixed elp root dir func

--- after/lsp/elp.lua
+++ after/lsp/elp.lua
@@ -1,30 +1,51 @@
+function find_ecss10_root(path)
+    local dir = vim.fs.dirname(path)
+
+    while dir do
+        local name = vim.fs.basename(dir)
+
+        if name == "ecss10" or name:match("^ecss10[_%.].+") then
+            return dir
+        end
+
+        local parent = vim.fs.dirname(dir)
+        if parent == dir then
+            break
+        end
+
+        dir = parent
+    end
+end
+
 return {
-  cmd = { 'elp', 'server' },
-  filetypes = { 'erlang' },
-  -- root_dir = '~/ecss10/',
 
-  root_dir = function(bufnr, on_dir)
-    local util = require("lspconfig.util")
-    local fname = vim.api.nvim_buf_get_name(bufnr)
+    cmd = { 'elp', 'server' },
+    filetypes = { 'erlang' },
+    -- root_dir = '~/ecss10/',
 
-    -- Check if LSP already attached to this buffer
-    local clients = vim.lsp.get_clients({ bufnr = bufnr })
-    if #clients > 0 then
-      return -- skip (don't call on_dir → no new LSP)
-    end
+    root_dir = function(bufnr, on_dir)
+        local fname = vim.api.nvim_buf_get_name(bufnr)
 
-    -- 1. Your custom rule
-    if fname:find("ecss10") then
-      on_dir(vim.fn.expand("~/ecss10"))
-      return
-    end
+        local clients = vim.lsp.get_clients({ bufnr = bufnr })
+        if #clients > 0 then
+            return
+        end
 
-    -- 2. Fallback to normal root pattern behavior
-    local root = util.root_pattern(".git", "rebar.config", "mix.exs")(fname)
+        -- ecss10, ecss10_2, ecss10.2, ecss10_test, ...
+        local root = find_ecss10_root(fname)
 
-    if root then
-      on_dir(root)
-    end
-  end,
-  root_markers = {},
+        if root then
+            on_dir(root)
+            return
+        end
+
+        -- Normal project detection
+        local util = require("lspconfig.util")
+        root = util.root_pattern(".git", "rebar.config", "mix.exs")(fname)
+
+        if root then
+            on_dir(root)
+        end
+    end,
+    root_markers = {},
 }
